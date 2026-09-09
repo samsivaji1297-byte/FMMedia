5af8e9b848e8f70dc368f78b42293120928c97ea
samsivaji1297-byte <samsivaji1297@gmail.com>
2026-09-09 10:40:09 +1000

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,44 +1,13 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const ipfactory = defineCollection({
-  type: 'content',
+const blog = defineCollection({
   schema: z.object({
     title: z.string(),
-    description: z.string().optional(),
-    date: z.string().optional(),
-  }),
-});
-
-const writingfactory = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    date: z.string().optional(),
-  }),
-});
-
-const sovereignos = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    date: z.string().optional(),
-  }),
-});
-
-const blueprints = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
+    description: z.string(),
     date: z.string().optional(),
   }),
 });
 
 export const collections = {
-  ipfactory,
-  writingfactory,
-  sovereignos,
-  blueprints,
+  blog,
 };
