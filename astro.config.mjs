2a564e25606a32348885d6c53907d85f037d16b6
samsivaji1297-byte <samsivaji1297@gmail.com>
2026-09-09 12:41:44 +1000

    Change output type from 'hybrid' to 'static'

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://example.com',
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   integrations: [mdx(), sitemap()],
   fonts: [
