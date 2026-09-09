c278840b7a96a7030ad4f2010f455ddaf5fdd22b
samsivaji1297-byte <samsivaji1297@gmail.com>
2026-09-09 12:40:32 +1000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://example.com',
