4f1c47a3bdc0955ad21d29114d274ee2d3ea08b3
samsivaji1297-byte <samsivaji1297@gmail.com>
2026-09-09 13:39:55 +1000

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,19 @@
 import { defineCollection, z } from 'astro:content';
 
-// Flexible date parser that accepts Strings, Date objects, or YAML timestamps
-const flexibleDate = z.union([z.string(), z.date()]).transform((val) => new Date(val));
-
-const baseSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  date: flexibleDate,
-  pubDate: flexibleDate.optional(),
+const blog = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.any(),
+    pubDate: z.any().optional(),
+    slug: z.string().optional(),
+  }),
 });
 
 export const collections = {
-  blog: defineCollection({ schema: baseSchema }),
-  ipfactory: defineCollection({ schema: baseSchema }),
-  blueprints: defineCollection({ schema: baseSchema }),
-  sovereignos: defineCollection({ schema: baseSchema }),
-  writingfactory: defineCollection({ schema: baseSchema }),
+  blog,
+  ipfactory: blog,
+  blueprints: blog,
+  sovereignos: blog,
+  writingfactory: blog,
 };
